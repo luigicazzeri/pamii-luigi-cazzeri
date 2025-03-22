2c0ad003942874f795e320ae519775867ea6d5bb
luigicazzeri <luigi.cazzeri0310@gmail.com>
2025-03-21 22:26:02 -0300

    Aula sobre firebase

--- aula04/app/index.tsx
+++ aula04/app/index.tsx
@@ -1,15 +1,56 @@
-import { Text, View } from "react-native";
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export default function Index() {
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAytG3_O1O7bVWrUSTIK4sbk-RspAbEEI8",
+  authDomain: "meu-primeiro-firebase-df50c.firebaseapp.com",
+  projectId: "meu-primeiro-firebase-df50c",
+  storageBucket: "meu-primeiro-firebase-df50c.firebasestorage.app",
+  messagingSenderId: "547302080103",
+  appId: "1:547302080103:web:689b8f694a77ce0d415ec2"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+import React, {useEffect, useState} from 'react';
+import {View, Text, FlatList} from 'react-native';
+
+export default function App() {
+  const [nomes, setNomes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const nomesCollection = firebase.firestore().collection('Nomes');
+      const snapshot = await nomesCollection.get();
+
+      const data = [];
+      snapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+      });
+
+      setNomes(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    <View style={{ flex: 1, justifyContent: 'center',
+    aligntItems: 'center'}}>
+        <Text>Lista de Nomes:</Text>
+        <FlatList
+          data={nomes}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item}) => (
+            <View>
+              <text>{item.Nome} {item.Sobrenome}</text>
+            </View>
+          )}
+      </View>
+  )
+
 }
